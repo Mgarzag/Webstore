e97a4a0b8acd33a8d736f81dd0d57cd248609c38
Marcel <marcel.garza09@gmail.com>
2020-11-03 19:30:56 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+//hoc
+import WithAdminAuth from './hoc/withAdminAuth';
+
+//layouts
+import MainLayout from './layouts/MainLayout';
+import HomepageLayout from './layouts/HomepageLayout';
+import AdminLayout from './layouts/AdminLayout';
+
+//pages
+import Homepage from './pages/Homepage';
+import Search from './pages/Search';
+import Registration from './pages/Registration';
+import Login from './pages/Login';
+import Admin from './pages/Admin';
+import './default.scss';
+
+function App() {
+  return (
+    <div className="App">
+        <Switch>
+          <Route exact path="/" render={() => (
+            <HomepageLayout>
+              <Homepage />
+            </HomepageLayout>
+          )} />
+          <Route exact path="/search" render={() => (
+          <MainLayout>
+            <Search />
+          </MainLayout>
+        )} />
+          <Route path="/registration" render={() => (
+            <MainLayout>
+              <Registration />
+            </MainLayout>
+          )} />
+          <Route path="/login" render={() => (
+            <MainLayout>
+              <Login />
+            </MainLayout>
+          )} />
+           <Route path="/admin" render={() => (
+          <WithAdminAuth>
+            <AdminLayout>
+              <Admin />
+            </AdminLayout>
+          </WithAdminAuth>
+        )} />
+        </Switch>
+    </div>
+  );
+}
+
+export default App;
